25feedaf5a3505f5269103ee83c27920ed6f976e
Nikolai <rivennike@gmail.com>
2021-01-25 08:35:44 -0800

    added all files

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+
+// This App component is the top-level component which will be common
+// across all the different pages.
+// You can use this App component to keep state when navigating between pages,
+// for example.
+// If you want some CSS to be loaded by every page,
+// Next.js has support for that as well.
